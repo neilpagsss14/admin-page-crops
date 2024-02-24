6d09fd1341e3e9cd3dcdb7084b490aace897351f
neilpagsss14 <neilpagara50@gmail.com>
2024-02-24 17:38:17 +0800

    added initial logo for navbar

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,10 +3,19 @@ import React from "react";
 function LandingPage() {
   return (
     <div className="bg-green-100 flex flex-row items-center justify-around h-screen w-screen">
+      <div className="fixed top-0 left-0">
+        <a href="/">
+          <img
+            className="md:h-20 md:w-auto -my-auto scale-50 cursor-pointer"
+            src="/src/assets/images/thesis.png"
+          />
+        </a>
+      </div>
       <div>
         <img
-          className="sm:h-40 h-44 md:h-72 mt-30 md:mt-0 lg:mt-0 sm:mt-0"
+          className=" sm:h-40 h-44 md:h-72 mt-30 md:mt-0 lg:mt-0 sm:mt-0"
           src="/src/assets/images/crops.png"
+          // src="https://lms-sg-application-bucket.s3.ap-southeast-1.amazonaws.com/public/prd/LAF_Text.png"
         />
       </div>
       <div className="flex items-center">
@@ -33,7 +42,7 @@ function LandingPage() {
             </div>
           </div>
           <div>
-            <button className="bg-green-500 py-3 px-0 rounded-xl font-poppins-bold hover:text-white w-full">
+            <button className="bg-green-500 hover:bg-green-700 py-3 px-10 rounded-xl text-sm font-poppins-bold hover:text-white w-full delay-1500 duration-100 ease-in-out">
               Log In
             </button>
             <span className="text-green-500 font-poppins text-sm mt-3 cursor-pointer">
@@ -41,11 +50,11 @@ function LandingPage() {
             </span>
           </div>
           <div className="flex mt-6 justify-center">
-            <span className="font-poppins">Don't have an account?</span>
+            <span className="font-poppins text-xs">Don't have an account?</span>
           </div>
           <div className="flex mt-3 flex-col gap-x-8 md:gap-y-2">
-            <button className="bg-green-500 hover:bg-green-700 rounded-xl px-10 py-3 text-sm font-poppins-bold  hover:text-white delay-1500 duration-300 ease-in-out">
-              REGISTER AS ADMIN
+            <button className="bg-green-500 hover:bg-green-700 rounded-xl px-10 py-3 text-sm font-poppins-bold  hover:text-white delay-1500 duration-100 ease-in-out">
+              Apply as Admin
             </button>
           </div>
         </div>
